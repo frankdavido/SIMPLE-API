bc3f70e50038fbbe29f83037a61907c00ab586d2
frankdavid <ukahfranklin@yahoo.com>
2022-02-21 00:02:02 +0100

    Revert changes to settings.php

--- api/settings.php
+++ api/settings.php
@@ -1,7 +1,8 @@
 <?php
-
+//// ini_set('display_errors', 1);
+//// error_reporting(E_ALL);
 #region
-/*
+/**
  * NOTE: Do not change anything in this region
  * */
 if (!defined('DOCUMENT_ROOT')) {
@@ -11,45 +12,44 @@ if (!defined('HOSTNAME')) {
     define('SERVER_NAME', str_replace('www.', '', $_SERVER['SERVER_NAME']));
     define('HTTP_HOST', $_SERVER['HTTP_HOST']);
     define('HOSTNAME', explode('.', SERVER_NAME, 2)[0]);
-    define('REQUEST_URI', $_SERVER['REQUEST_URI']);
 }
-/*
+/**
  * Get URI elements.
  * @return array
  */
 function getUriSegments():array
 {
-    $uri = parse_url(REQUEST_URI, PHP_URL_PATH);
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $uri = explode('/', $uri);
     return $uri;
 }
 #endregion
 
 #region
-/*
- * You may alter this constants below as per your needs
+/**
+ * ----------------------- You may alter these constants below as per your needs ----------------------
  */
-/*
+/**
  * Class register for constants used in this site
  * */
 class CONSTANTS
 {
-    /*
+    /**
      * Database connection Parameters
     */
     public const DATABASE_HOST  = 'localhost';              // May be localhost or IP of the database server.
-    public const DATABASE_NAME  = 'testapi';                // Change to real database name
-    public const DATABASE_USER  = 'user';                   // Change to real database user
-    public const DATABASE_PASSWORD  = 'pass';// Change to real database password
-    /*
+    public const DATABASE_NAME  = 'user_testapi';                // Change to real database name
+    public const DATABASE_USER  = 'user_elipheleh';                   // Change to real database user
+    public const DATABASE_PASSWORD  = 'bEsyG1Z7N17ekw8K';// Change to real database password
+    /**
      * Whether to use persistent connection to database. Makes the application faster.
     */
     public const PERSISTENT_DATABASE_CONNECTION = false;    // Boolean [true of false]. Default is false
-    /*
+    /**
      * Do you want my script to automatically create and populate some Test data for you inside the database?
      */
     public const CREATE_TEST_DATA = true;                   // Boolean [true of false].
-    /*
+    /**
      * Default Timezone
      * For more list of timezones,
      * SEE: https://www.w3schools.com/php/php_ref_timezones.asp
@@ -59,7 +59,7 @@ class CONSTANTS
 
 
 #endregion
-
+//
 
 date_default_timezone_set(constants::TIMEZONENAME);
 require_once DOCUMENT_ROOT . '/db/dbconn.php';
